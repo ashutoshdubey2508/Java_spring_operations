fb8e840f93ef5221a80487bdd1422ba55e50d632
ashutoshdubey2508 <ashutoshdubey2508@gmail.com>
2023-08-29 07:57:15 +0530

    29aug

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        Student student1 = (Student) context.getBean("Student1");
+
+        System.out.println(student1);
+
     }
 }
